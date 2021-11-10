a4cabe1c1ff840602594bb1f38323b56a248fdc3
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2021-11-09 21:01:30 -0600

    created objects and lwcs for resume base volunteer hx and work hx

---
+++ force-app/main/default/lwc/experienceCard/experienceCard.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ExperienceCard extends LightningElement {
+    @api experience;
+}
---
+++ force-app/main/default/lwc/resume/resume.js
@@ -0,0 +1,43 @@
+import { LightningElement, track } from 'lwc';
+import getEmployers from '@salesforce/apex/resumeHelper.getEmployers';
+
+export default class Resume extends LightningElement {
+
+    @track experience;
+    @track error;
+    @track altExp;
+    @track workHx;
+    @track volunteerHx;
+    @track educationHx;
+
+    connectedCallback(){
+        getEmployers()
+        .then(result => {
+            this.experience = result;
+            this.workHx = result.filter(this.filterWorkHx);
+            this.volunteerHx = result.filter(this.filterVolunteerHx);
+            this.educationHx = result.filter(this.filterEducationHx);
+            this.error = undefined;    
+        })
+        .catch(error => {
+            this.error = error;
+            this.experience = undefined;
+            this.workHx = undefined;
+            this.volunteerHx = undefined;
+            this.educationHx = undefined;
+        });
+    }
+
+    filterWorkHx(item){
+        return item.Type == "Employer";
+    }
+
+    filterVolunteerHx(item){
+        return item.Type == "Volunteer Organization";
+    }
+
+    filterEducationHx(item){
+        return item.Type == "Educational Institution";
+    }
+
+}
